a0fb0655eb33a527c477d7a5795cb9493fb59b45
Daniel <dal279@scarletmail.rutgers.edu>
2024-02-15 21:25:08 -0500

    some changes

--- mymalloc.c
+++ mymalloc.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include "mymalloc.h"
 
-#define MEMLENGTH 512
+#define MEMLENGTH 4096
 static double memory[MEMLENGTH];
 
 //malloc implementation 
+void *mymalloc(size_t size){
 
+}
 
 
