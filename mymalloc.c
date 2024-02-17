53f4a37a4aa5e60f81b47ccd20789f4eb11ffde4
Daniel <dal279@scarletmail.rutgers.edu>
2024-02-17 16:03:39 -0500

    i

--- mymalloc.c
+++ mymalloc.c
@@ -32,10 +32,7 @@ void *mymalloc(size_t size, char *file, int line) {
     size = align(size);                                         // ensures allignment 
     char *heap = (char*)memory;                                 // pointer to the start of memory for byte-width operations
     if(memory[0] != '/0'){                                      // checks if memory is initiazlied, proceeds to other checks and allocation 
-        
-        else{
-
-        }                   
+                 
 
     }                                  
     else{
