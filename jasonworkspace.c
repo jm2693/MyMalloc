9b9d25ae996ea5e29cffdf2e51ff8cd3f04ce58a
Jason Merchan <jm2693@scarletmail.rutgers.edu>
2024-02-18 01:54:09 -0500

    more mymalloc and helper methods

--- jasonworkspace.c
+++ jasonworkspace.c
@@ -6,10 +6,60 @@
 static double memory[MEMLENGTH];
 
 typedef struct metadata{
-    int size;                       // size of memory chunk (including header)
-    int in_use;                     // flag to see if chunk is allocated (1 - allocated, 0 - not allocated)
+    int size;                       // size of memory curr_header (including header)
+    int in_use;                     // flag to see if curr_header is allocated (1 - allocated, 0 - not allocated)
     //struct metadata *next;        // pointer to the next available free space  (not going with this method because increases memory usage by a lot and not necessary for small data sizes)
 } metadata;
+
+size_t align(size_t size) {                                     // method to align everything as 8-byte aligned
+    return (size+7) & ~7;                                       // uses addition and bitwise and to round up to nearest multiple of 8
+}
+
+void next_chunk() {
+    
+}
+
+
+//malloc implementation 
+void *mymalloc(size_t size, char *file, int line) {
+    if(size > MEMLENGTH || size <= 0){                          // checks if size is bigger than 4096 bytes or less than or equal to 0 
+        printf("Error at %s:%d: Invalid size\n", file, line);
+        return NULL;
+    }
+    size = align(size);                                         // ensures allignment 
+    
+    metadata *chunk = (metadata*)memory;                        // storing size and if in_use within metadata struct
+    double *payload = NULL;                                     // ptr to payload. initially points to nothing 
+    double *start_ptr = memory;                                 // pointer to the start of memory  
+    double *end_ptr = &memory[MEMLENGTH-1];                     // pointer to end of memory 
+
+    while (start_ptr <= end_ptr) {                              // scans through entire heap array until it ends
+        int *curr_header = (int*)start_ptr;                     // points to start of memory on first run. int pointer to get metadata values                                      
+        chunk->size = curr_header[0];                                    
+        chunk->in_use = curr_header[1];
+
+        if (chunk->size == 0 && chunk->in_use == 0) {           // first metadata ints are 0, i.e. not allocated and size of 0 (not initialized)
+            curr_header[0] = size + sizeof(metadata);           // allocated size asked for plus size of its own header
+            curr_header[1] = 1;                                 // in_use = 1 to represent curr_header being allocated
+            payload = start_ptr + 1;                            // increment current pointer to one following start
+            
+        } 
+        if (chunk->size >= size + sizeof(metadata) && chunk->in_use == 0) {         
+            curr_header[0] = size + sizeof(metadata);                  // allocated size asked for plus size of its own header
+            curr_header[1] = 1;                                        // in_use = 1 to represent curr_header being allocated
+            
+        }
+        if ()
+    }
+    
+}
+
+
+void myfree(void *ptr, char *file, int line) {
+
+}
+
+
 
 
 // int get_size(metadata ptr) {
@@ -20,53 +70,3 @@ typedef struct metadata{
 // size_t get_in_use(metadata *ptr) {
 //     return ptr->in_use;                 // returns in_use flag
 // }
-
-
-size_t align(size_t size) {                                     // method to align everything as 8-byte aligned
-    return (size+7) & ~7;                                       // uses addition and bitwise and to round up to nearest multiple of 8
-}
-
-void init_heap() {
-    metadata *init_chunk = (metadata*)memory;                   // creating a metadata pointer to point to memory
-    init_chunk->size = MEMLENGTH-sizeof(metadata);              // size of initial chunk is entire heap (including header)
-    init_chunk->in_use = 0;                                     // initially has nothing allocated
-}
-
-//malloc implementation 
-void *mymalloc(size_t size, char *file, int line) {
-    if(size > MEMLENGTH || size <= 0){                          // checks if size is bigger than 4096 bytes or less than or equal to 0 
-        printf("Error at %s:%d: Invalid size\n", file, line);;
-        return NULL;
-    }
-    size = align(size);                                         // ensures allignment 
-    
-    double *payload = NULL;
-    double *start_ptr = memory;                                 // pointer to the start of memory  
-    double *end_ptr = &memory[MEMLENGTH-1];                     // pointer to end of memory 
-
-    while (start_ptr <= end_ptr) {                               // scans through entire heap array until it ends
-        int *chunk = (int*)start_ptr;                            // points to start of memory on first run. int pointer to get metadata values
-        metadata init;                                           // storing size and if in_use within metadata struct
-        init.size = chunk[0];                                    
-        init.in_use = chunk[1];
-
-        if(init.size == 0 && init.in_use == 0) {                 // first metadata ints are 0, i.e. not allocated and size of 0 (not initialized)
-            chunk[0] = size + sizeof(metadata);                  // allocated size asked for plus size of its own header
-            chunk[1] = 1;                                        // in_use = 1 to represent chunk being allocated
-            payload = start_ptr + 1;                            // increment current pointer to one following start
-            
-        } 
-        else if (init.size >= size + sizeof(metadata) && init.in_use == 0) {         
-            chunk[0] = size + sizeof(metadata);                  // allocated size asked for plus size of its own header
-            chunk[1] = 1;                                        // in_use = 1 to represent chunk being allocated
-            
-        }
-    }
-    
-}
-
-
-void myfree(void *ptr, char *file, int line) {
-
-}
-
