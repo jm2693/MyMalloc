23163f8d63fdec35485132b36da630593f3c12d6
Jason Merchan <jm2693@scarletmail.rutgers.edu>
2024-02-17 22:41:27 -0500

    some of mymalloc

--- mymalloc.c
+++ mymalloc.c
@@ -5,19 +5,30 @@
 #define MEMLENGTH 512
 static double memory[MEMLENGTH];
 
-typedef struct metaData{
+typedef struct metadata{
     int size;                       // size of memory chunk (including header)
     int in_use;                     // flag to see if chunk is allocated (1 - allocated, 0 - not allocated)
-    //struct metaData *next;          // pointer to the next available free space 
-} metaData;
+    //struct metadata *next;        // pointer to the next available free space  (not going with this method because increases memory usage by a lot and not necessary for small data sizes)
+} metadata;
+
+
+// int get_size(metadata ptr) {
+//     metadata *get_size = &ptr;
+//     return get_size->size;               // return size of metadata+payload
+// }
+
+// size_t get_in_use(metadata *ptr) {
+//     return ptr->in_use;                 // returns in_use flag
+// }
+
 
 size_t align(size_t size) {         // method to align everything as 8-byte aligned
     return (size+7) & ~7;           // uses addition and bitwise and to round up to nearest multiple of 8
 }
 
 void init_heap() {
-    metaData *init_chunk = (metaData*)memory;                   // creating a metadata pointer to point to memory
-    init_chunk->size = MEMLENGTH-sizeof(metaData);              // size of initial chunk is entire heap (including header)
+    metadata *init_chunk = (metadata*)memory;                   // creating a metadata pointer to point to memory
+    init_chunk->size = MEMLENGTH-sizeof(metadata);              // size of initial chunk is entire heap (including header)
     init_chunk->in_use = 0;                                     // initially has nothing allocated
 }
 
@@ -29,11 +40,22 @@ void *mymalloc(size_t size, char *file, int line) {
     }
     size = align(size);                                         // ensures allignment 
     
-    double *heap_ptr = memory;                                  // pointer to the start of memory  
+    double *curr_ptr = NULL;
+    double *start_ptr = memory;                                 // pointer to the start of memory  
     double *end_ptr = &memory[MEMLENGTH-1];                     // pointer to end of memory 
 
-    while (heap_ptr <= end_ptr) {                               // scans through entire heap array until it ends
-        
+    while (start_ptr <= end_ptr) {                               // scans through entire heap array until it ends
+        int *chunk = (int*)start_ptr;                            // points to start of memory on first run. int pointer to get metadata values
+        metadata init;                                           // storing size and if in_use within metadata struct
+        init.size = chunk[0];
+        init.in_use = chunk[1];
+
+        if(init.size == 0 && init.in_use == 0) {                 // first metadata ints are 0, i.e. not allocated and size of 0 (not initialized)
+            chunk[0] = size + sizeof(metadata);                  // allocated size asked for plus size of its own header
+            chunk[1] = 1;                                        // in_use = 1 to represent chunk being allocated
+        } else if (init.size != 0 && init.in_use != 0) {
+            chunk = chunk + init.size;
+        }
     }
     
 }
