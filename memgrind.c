ff44c3552d114e6828586e7569c3dff7b9c2fb9d
Jason Merchan <jm2693@scarletmail.rutgers.edu>
2024-02-19 17:03:39 -0500

    memgrind updates

--- memgrind.c
+++ memgrind.c
@@ -9,7 +9,7 @@
 
 void test1() {
     void *memgrind_arr[4096];                                           // a test array used to store pointers to allocated memory
-    for(int i = 0; i < 120; i++) {                                      // will repeat 120 times
+    for (int i = 0; i < 120; i++) {                                      // will repeat 120 times
         memgrind_arr[i] = malloc(1);                                    // allocating malloc at a position in memgrind_arr for testing
         if (DEBUG) {
             printf("memory allocated at %p\n", &memgrind_arr[i]);       // printing malloc statement
@@ -21,10 +21,39 @@ void test1() {
     }
 }
 
-void test2(){
+void test2() {
+    void *memgrind_arr[4096];
+    for (int i = 0; i < 120; i++) {
+        memgrind_arr[i] = malloc(1);
+        if (DEBUG) {
+            printf("memory allocated at %p\n", &memgrind_arr[i]);       // printing malloc statement
+        } 
+    }
+    for (int i = 0; i < 120; i++) {
+        free(memgrind_arr[i]);
+        if (DEBUG) {
+            printf("memory freed at %p\n", &memgrind_arr[i]);           // printing free statement
+        } 
+    }
+}
+
+void test3() {
+    void *memgrind_arr[120];
+    
+
+
+}
+
+void test4() {
     void *memgrind_arr[4096];
     memgrind_arr[0] = malloc(4099);
 }
+
+
 int main(int argc, char* argv[]) {
-    test1();
+    for (int i = 0; i < 50; i++) {
+        test1();
+        test2();
+        test3();
+    }
 }
