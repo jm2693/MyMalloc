52178c2421188fd0752307bd92decca6ee107460
Jason Merchan <jm2693@scarletmail.rutgers.edu>
2024-02-20 16:48:40 -0500

    new memgrind

--- memgrind.c
+++ memgrind.c
@@ -21,6 +21,8 @@ void test1() {
     }
     memgrind_arr[18] = malloc(1000);                                    // should be able to be allocated 
     printf("memory allocated at %p\n", memgrind_arr[18]);
+    free(memgrind_arr[18]);
+    printf("memory freed at %p\n", memgrind_arr[18]);           // printing free statement
 }
 
 void test2() {
@@ -37,67 +39,119 @@ void test2() {
         if (DEBUG) {
             printf("memory freed at %p\n", memgrind_arr[i]);           // printing free statement
         } 
-    }
-    //memgrind_arr[18] = malloc(4050);                                    // should be able to be allocated 
+    } 
 }
 
-// void test3() {
-//     void *memgrind_arr[120];
-//     while 
 
-
-
-// }
 void test3() {
+    void* memgrind_arr[120] = { NULL };
+        int allocated_count = 0;
+        srand(time(NULL)); // Seed for random number generator
+
+        for (int i = 0; i < 120; ++i) {
+            int choice = rand() % 2; // Random choice between 0 and 1
+
+            if (choice == 0 && allocated_count < 120) {
+                // Allocate a 1-byte object and add the pointer to the array
+                memgrind_arr[allocated_count] = malloc(1);
+                if (memgrind_arr[allocated_count] == NULL) {
+                    fprintf(stderr, "memory allocation failed\n");
+                    break;
+                }
+                printf("memory allocated at index %d: pointer to address %p\n", allocated_count, memgrind_arr[i]);
+                allocated_count++;
+            } else if (choice == 1 && allocated_count > 0) {
+                // Deallocate a previously allocated object
+                free(memgrind_arr[allocated_count - 1]);
+                memgrind_arr[allocated_count - 1] = NULL;
+                printf("memory deallocated at index %d: pointer to address %p\n", allocated_count - 1, memgrind_arr[i-1]);
+                allocated_count--;
+            }
+        }
+
+        // Deallocate any remaining objects
+        for (int i = 0; i < allocated_count; ++i) {
+            free(memgrind_arr[i]);
+            printf("memory deallocated at index %d: pointer to address %p\n", i, memgrind_arr[i]);
+        }
+    }
+
+
+
+
+void test4() {
     void* ptr = malloc(1);
     printf("memory allocated at %p\n", ptr); 
     ptr = malloc(3);
     printf("memory allocated at %p\n", ptr); 
-
+    free(ptr);
+    if (DEBUG) {
+        printf("memory freed at %p\n", ptr);           // printing free statement
+    } 
+    free(ptr);
+    if (DEBUG) {
+        printf("memory freed at %p\n", ptr);           // printing free statement
+    } 
 }
 
-void test4() {
-    void *memgrind_arr[5];
+void test5() {
+    void *memgrind_arr[2];
     memgrind_arr[0] = malloc(4099);                                     // trys to allocate a memory that's too large, return error 
-    free(memgrind_arr[0]);                                              // trys to free, should return error as it was not put into memory
-}
-
-void test5(){
-    void *memgrind_arr[10];
-    memgrind_arr[0] = malloc(0);                                        // trys to allocate a memory that's too large, return error 
     free(memgrind_arr[0]);                                              // trys to free, should return error as it was not put into memory
 }
 
 void test6(){
     void *memgrind_arr[10];
-    for(int i = 0; i < 3; i++){
+    memgrind_arr[0] = malloc(0);                                        // trys to allocate a memory that's too large, return error 
+    free(memgrind_arr[0]);                                              // trys to free, should return error as it was not put into memory
+}
+
+void test7(){
+    void *memgrind_arr[10];
+    for(int i = 0; i < 4; i++){
         memgrind_arr[i] = malloc(1000);
         if (DEBUG) {
             printf("memory allocated at %p\n", memgrind_arr[i]);       // printing malloc statement
         } 
     }
     free(memgrind_arr[0]);
+    if (DEBUG) {
+            printf("memory freed at %p\n", memgrind_arr[0]);       // printing malloc statement
+        } 
     free(memgrind_arr[2]);
-    memgrind_arr[3] = malloc(2504);                                     // should fail
+    if (DEBUG) {
+            printf("memory freed at %p\n", memgrind_arr[2]);       // printing malloc statement
+        } 
+    memgrind_arr[3] = malloc(2000);                                     // should fail
+}
+
+void test8() {
+    void* ptr;
+    free(ptr);
 }
 
 int main(int argc, char* argv[]) {
 
-    test2();
-    test4();
-    test5();
-    test6();
+    //test1();
+    //test2();
+    test3();
+    //test4();
+    //test5();
+    //test6();
+    //test7();
+    //test8();
+
     // for (int i = 0; i < 50; i++) {
-    //     test1();
+    //     // test6();
     //     //test3();
     // }
-//     char* arr[5];
-//     arr[1] = malloc(1);
-//     printf("mem allocated at %p\n", arr[1]);
-//     arr[2] = malloc(2);
-//     printf("mem allocated at %p\n", arr[2]);
-//     free(arr[1]);
-//     printf("mem deallocated at %p\n", arr[1]);
-//     free(arr[2]);
-//     printf("mem deallocated at %p\n", arr[2]);
- }
+    // char* arr[5];
+    // arr[1] = malloc(1);
+    // printf("mem allocated at %p\n", arr[1]);
+    // arr[2] = malloc(2);
+    // printf("mem allocated at %p\n", arr[2]);
+    // free(arr[1]);
+    // printf("mem deallocated at %p\n", arr[1]);
+    // free(arr[2]);
+    // printf("mem deallocated at %p\n", arr[2]);
+}
