b24148bd1f14dd287bf51ea9b5f5ed16edfd5563
Jason Merchan <jm2693@scarletmail.rutgers.edu>
2024-02-20 01:21:09 -0500

    progress

--- mymalloc.c
+++ mymalloc.c
@@ -59,7 +59,10 @@ void *mymalloc(size_t size, char *file, int line) {
         if (chunk->chunk_size == 0 && chunk->in_use == 0) {     // first metadata ints are 0, i.e. not allocated and size of 0 (not initialized)
             chunk->chunk_size = size + sizeof(metadata);        // allocated size asked for plus size of its own header
             chunk->in_use = 1;                                  // in_use = 1 to represent curr_header being allocated
-            payload = start_ptr + 1;                            // increment current pointer to one following start
+            printf("returning 4 address of %p\n", start_ptr);
+            start_ptr = next_chunk((metadata*)(start_ptr));    
+            printf("returning 3 address of %p\n", start_ptr);
+            payload = start_ptr;                            // increment current pointer to one following start
             printf("returning 1 address of %p\n", payload);
 
             // use init_next_chunk here with curr_header and size of metadata+available space afterwards (in ints)
@@ -67,10 +70,13 @@ void *mymalloc(size_t size, char *file, int line) {
             return (void*)payload;
             
         } 
+        start_ptr = next_chunk((metadata*)(start_ptr));    
         if (chunk->chunk_size - (size + sizeof(metadata)) >= 0) {         
             chunk->chunk_size = size + sizeof(metadata);        // allocated size asked for plus size of its own header
             chunk->in_use = 1;                                  // in_use = 1 to represent curr_header being allocated
-            payload = start_ptr + 1;
+            start_ptr = next_chunk((metadata*)(start_ptr));    
+            printf("returning 3 address of %p\n", start_ptr);
+            payload = start_ptr;
             printf("returning 2 address of %p\n", payload);
 
             // use init_next_chunk here with curr_header and size of metadata+available space afterwards (in ints)
